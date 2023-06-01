beae23dbdb3b643649bfb82c6c973c72912bf812
siona kaura <88344028+sionakaura@users.noreply.github.com>
2023-06-01 11:15:24 -0500

    Add files via upload

---
+++ EPHSScheduleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EPHSScheduleAppApp.swift
+//  EPHSScheduleApp
+//
+//  Created by 64001567 on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct EPHSScheduleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
